d0a52d78f6a75ae80aa635f737d97b9d8f617d48
Yotam Bar-On <tudmotu@gmail.com>
2014-01-03 15:34:29 +0200

    Refactored vcs-branch indicator in PS1; Fixed issues.

--- prompt.sh
+++ prompt.sh
@@ -1,4 +1,16 @@
 # Version Cotrol PS1 Helpers
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+PURPLE=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+RESET=$(tput sgr0)
+BOLD=$(tput bold)
+DIM=$(tput dim)
+
 __hg_branch() {
     hg branch 2> /dev/null | \
         awk '{print $1}'
@@ -25,36 +37,27 @@ __parse_svn_url() {
 __parse_svn_repository_root() {
     svn info 2>/dev/null | sed -ne 's#^Repository Root: ##p'
 }
-
-__set_prompt() {
-    local BLACK=$(tput setaf 0)
-    local RED=$(tput setaf 1)
-    local GREEN=$(tput setaf 2)
-    local YELLOW=$(tput setaf 3)
-    local BLUE=$(tput setaf 4)
-    local PURPLE=$(tput setaf 5)
-    local CYAN=$(tput setaf 6)
-    local WHITE=$(tput setaf 7)
-    local RESET=$(tput sgr0)
-    local BOLD=$(tput bold)
-    local DIM=$(tput dim)
+__vcs_ps1_template() {
+    echo -ne "(${CYAN}$1${WHITE}::$RED$2$RESET)"
+}
+__vcs_branches() {
     local git_branch=$(__git_branch)
     local hg_branch=$(__hg_branch)
     local svn_branch=$(__svn_branch)
     local prompt_line="";
 
     if [[ -n $git_branch ]]; then
-        prompt_line="$prompt_line(git::\[$RED\]$git_branch\[$RESET\])"
+        prompt_line="$prompt_line$(__vcs_ps1_template git $git_branch)"
     fi
     if [[ -n $hg_branch ]]; then
-        prompt_line="$prompt_line(hg::\[$RED\]$hg_branch\[$RESET\])"
+        prompt_line="$prompt_line$(__vcs_ps1_template hg $hg_branch)"
     fi
     if [[ -n $svn_branch ]]; then
-        prompt_line="$prompt_line(svn::\[$RED\]$svn_branch\[$RESET\])"
+        prompt_line="$prompt_line$(__vcs_ps1_template svn $svn_branch)"
     fi
 
-    prompt_line="$prompt_line\[$GREEN\]|\$(date +%k:%M:%S)|\
-\[$CYAN\]\u\[$YELLOW\]@\[$BLUE\]\w \[$RESET\]\[$GREEN\]\$\[$RESET\] "
-    PS1=$prompt_line
+    echo -e $prompt_line
 }
-__set_prompt
+
+export PS1="\$(__vcs_branches)\[$GREEN\]|\$(date +%k:%M:%S)|\
+\[$CYAN\]\u\[$YELLOW\]@\[$BLUE\]\w \[$RESET\]\[$GREEN\]\$\[$RESET\] "
